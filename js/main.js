fc41460d351b7111130196b9678d580cf425f79f
r0mm3L <melzalastra@gmail.com>
2016-08-16 22:25:49 +1200

    Add project files and folders

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$( "#thedeity" ).mouseover(function() {
+  $( "#sect1" ).show( "slow" );
+});
+$( "#thedeity" ).mouseout(function() {
+  $( "#sect1" ).fadeOut( "slow" );
+});
+
+$( "#witness" ).mouseover(function() {
+  $( "#sect2" ).show( "slow" );
+});
+$( "#witness" ).mouseout(function() {
+  $( "#sect2" ).fadeOut( "slow" );
+});
+$( "#flesh" ).mouseover(function() {
+  $( "#sect3" ).show( "slow" );
+});
+$( "#flesh" ).mouseout(function() {
+  $( "#sect3" ).fadeOut( "slow" );
+});
+$( "#testimony" ).mouseover(function() {
+  $( "#sect4" ).show( "slow" );
+});
+$( "#testimony" ).mouseout(function() {
+  $( "#sect4" ).fadeOut( "slow" );
+});
+$( "#ministry" ).mouseover(function() {
+  $( "#sect5" ).show( "slow" );
+});
+$( "#ministry" ).mouseout(function() {
+  $( "#sect5" ).fadeOut( "slow" );
+});
